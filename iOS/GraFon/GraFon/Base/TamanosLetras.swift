48a806935397fbf5c648d8b39efc779f60769297
SamArtGS <samuelarturo@icloud.com>
2021-06-23 11:03:50 -0500

    feat: comentario Letras Tamaños

--- iOS/GraFon/GraFon/Base/TamanosLetras.swift
+++ iOS/GraFon/GraFon/Base/TamanosLetras.swift
@@ -11,18 +11,99 @@ import UIKit
 struct Tamanio {
     
     static let letraCafeTarjetasMenu: CGFloat = 14
-    
-    
-    
     static let letrasCafeBocina:CGFloat = 22
     static let letrasRosaBocina:CGFloat = 20
-    
-    
-    
     static let letrasCafeBloques: CGFloat = 20
     static let letrasRosasBloques: CGFloat = 20
-    
     static let letraPosicionBloque: CGFloat = 25
     static let letraExplicacion:CGFloat = 16
     static let letraFonema:CGFloat = 25
+    
 }
+
+
+/*
+ 
+ iPhone 5-5s-SE:
+    static let letraCafeTarjetasMenu: CGFloat = 15
+    static let letrasCafeBocina:CGFloat = 23
+    static let letrasRosaBocina:CGFloat = 19
+    static let letrasCafeBloques: CGFloat = 19
+    static let letrasRosasBloques: CGFloat = 15
+    static let letraPosicionBloque: CGFloat = 27
+    static let letraExplicacion:CGFloat = 18
+    static let letraFonema:CGFloat = 27
+ 
+ 
+ iPhone 6-6s-7-8:
+    static let letraCafeTarjetasMenu: CGFloat = 15
+    static let letrasCafeBocina:CGFloat = 23
+    static let letrasRosaBocina:CGFloat = 19
+    static let letrasCafeBloques: CGFloat = 19
+    static let letrasRosasBloques: CGFloat = 15
+    static let letraPosicionBloque: CGFloat = 27
+    static let letraExplicacion:CGFloat = 18
+    static let letraFonema:CGFloat = 27
+ 
+ iPhone 6P-6sP-7P-8P:
+    static let letraCafeTarjetasMenu: CGFloat = 15
+    static let letrasCafeBocina:CGFloat = 23
+    static let letrasRosaBocina:CGFloat = 19
+    static let letrasCafeBloques: CGFloat = 19
+    static let letrasRosasBloques: CGFloat = 15
+    static let letraPosicionBloque: CGFloat = 27
+    static let letraExplicacion:CGFloat = 18
+    static let letraFonema:CGFloat = 27
+ 
+ iPhone X-Xs-11Pro:
+    static let letraCafeTarjetasMenu: CGFloat = 15
+    static let letrasCafeBocina:CGFloat = 23
+    static let letrasRosaBocina:CGFloat = 19
+    static let letrasCafeBloques: CGFloat = 19
+    static let letrasRosasBloques: CGFloat = 15
+    static let letraPosicionBloque: CGFloat = 27
+    static let letraExplicacion:CGFloat = 18
+    static let letraFonema:CGFloat = 27
+ 
+ iPhone XsMax-11ProMax-12ProMax:
+    static let letraCafeTarjetasMenu: CGFloat = 15
+    static let letrasCafeBocina:CGFloat = 23
+    static let letrasRosaBocina:CGFloat = 19
+    static let letrasCafeBloques: CGFloat = 19
+    static let letrasRosasBloques: CGFloat = 15
+    static let letraPosicionBloque: CGFloat = 27
+    static let letraExplicacion:CGFloat = 18
+    static let letraFonema:CGFloat = 27
+ 
+ iPhone 11-12-12Pro:
+    static let letraCafeTarjetasMenu: CGFloat = 15
+    static let letrasCafeBocina:CGFloat = 23
+    static let letrasRosaBocina:CGFloat = 19
+    static let letrasCafeBloques: CGFloat = 19
+    static let letrasRosasBloques: CGFloat = 15
+    static let letraPosicionBloque: CGFloat = 27
+    static let letraExplicacion:CGFloat = 18
+    static let letraFonema:CGFloat = 27
+ 
+ iPhone 12mini:
+    static let letraCafeTarjetasMenu: CGFloat = 15
+    static let letrasCafeBocina:CGFloat = 23
+    static let letrasRosaBocina:CGFloat = 19
+    static let letrasCafeBloques: CGFloat = 19
+    static let letrasRosasBloques: CGFloat = 15
+    static let letraPosicionBloque: CGFloat = 27
+    static let letraExplicacion:CGFloat = 18
+    static let letraFonema:CGFloat = 27
+ 
+ iPad:
+    static let letraCafeTarjetasMenu: CGFloat = 15
+    static let letrasCafeBocina:CGFloat = 23
+    static let letrasRosaBocina:CGFloat = 19
+    static let letrasCafeBloques: CGFloat = 19
+    static let letrasRosasBloques: CGFloat = 15
+    static let letraPosicionBloque: CGFloat = 27
+    static let letraExplicacion:CGFloat = 18
+    static let letraFonema:CGFloat = 27
+ */
+
+//SKU: 20210623
